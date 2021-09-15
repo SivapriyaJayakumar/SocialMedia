8a59b52a7fd0c20a39a849452e2da27dcbf2ae19
SivapriyaJayakumar <68330216+SivapriyaJayakumar@users.noreply.github.com>
2021-09-15 11:22:19 +0530

    not found - push

--- src/Loginmodal.js
+++ src/Loginmodal.js
@@ -2,6 +2,7 @@ import './App.css';
 import App from "./App";
 import Profile from "./Profile";
 import Login from './LoginPage';
+import NotFound from './notfound'
 import {Switch,Route,Redirect} from 'react-router-dom';
 import Myposts from './myposts';
 
@@ -14,8 +15,8 @@ function LoginNavigator(){
    
     <Route path="/home"><App /></Route>
     <Route exact path="/profile" ><Profile/></Route>
-    <Route exact path="/posts" ><Myposts/></Route>
-    <Route path="/"><Login/></Route>
+    <Route exact path="/"><Login/></Route>
+    <Route path="/"><NotFound/></Route>
          
 </Switch>
 </div>
---
+++ src/notfound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound(){
+    return(
+        <div className="notfound">
+            Page is under construction....
+            Visit after some time!
+        </div>
+    )
+}
+export default NotFound;
